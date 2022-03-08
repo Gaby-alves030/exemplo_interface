7c176b37049404b977e908a972e559e534fa2360
Gaby <gaabyrocha574@gmail.com>
2022-03-08 19:16:37 +0000

    veiculo

---
+++ Carro.java
@@ -0,0 +1,27 @@
+public class Carro implements Motor, Veiculo {
+
+    @Override
+    public String getFabricante() {
+        // TODO Auto-generated method stub
+        return "FIAT";
+    }
+
+    @Override
+    public String getModelo() {
+        // TODO Auto-generated method stub
+        return "SI";
+    }
+
+    @Override
+    public int getId() {
+        // TODO Auto-generated method stub
+        return 666;
+    }
+
+    @Override
+    public String getNome() {
+        // TODO Auto-generated method stub
+        return "Pampa";
+    }
+
+}
